3def9a56c682a8db7cf1e39d2320e0016f1f160e
Jonathan Wage <jwage@bambino.local>
2009-11-13 16:32:56 -0500

    0

---
+++ tests/SkeletonProject/Tests/SampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SkeletonProject\Tests;
+
+use \SkeletonProject\Api,
+    \SkeletonProject\Client\HTTP;
+
+require_once __DIR__ . '/TestInit.php';
+
+class SampleTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSample()
+    {
+        $this->assertTrue(true);
+    }
+}
