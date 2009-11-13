7ce5b5e268ab3a7fb4ee4d5a8b1bba8e9c378fab
Jonathan Wage <jwage@bambino.local>
2009-11-13 18:32:34 -0500

    -

--- tests/SkeletonProject/Tests/SampleTest.php
+++ tests/SkeletonProject/Tests/SampleTest.php
@@ -2,9 +2,6 @@
 
 namespace SkeletonProject\Tests;
 
-use \SkeletonProject\Api,
-    \SkeletonProject\Client\HTTP;
-
 require_once __DIR__ . '/TestInit.php';
 
 class SampleTest extends \PHPUnit_Framework_TestCase
